164d56bff2f92ab567aa1137dd38ac67b885d83d
Jonathan C <johnlejardinnier@gmail.com>
2017-11-29 22:14:35 +0100

    feat(Pipo): fix current pos on forward & backward

--- Pipo.py
+++ Pipo.py
@@ -52,14 +52,14 @@ class Pipo:
         )
 
     def forward(self):
-        self.CURRENT_POS = self.POS_BACKWARD
+        self.CURRENT_POS = self.POS_FORWARD
         for i,M in enumerate(self.Motors):
             (self.Motors[i]).forward()
         #self.__accelerate()
         self.__adapt_speed()
 
     def backward(self):
-        self.CURRENT_POS = self.POS_FORWARD
+        self.CURRENT_POS = self.POS_BACKWARD
         for i,M in enumerate(self.Motors):
             (self.Motors[i]).backward()
         #self.__accelerate()
